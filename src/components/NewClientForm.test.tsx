48d9200b3ebe79f7ee51b4b0a6acef76e2743d2e
Katariniss <echekina@gmail.com>
2022-01-25 20:21:02 +0500

    test(create): #5: create test for the case of submitting empty name

--- src/components/NewClientForm.test.tsx
+++ src/components/NewClientForm.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import NewClientForm from './NewClientForm';
 
 test('does not show name validation message initially', () => {
@@ -10,4 +10,17 @@ test('does not show name validation message initially', () => {
 
   const validationMessageElement = screen.queryByText(`Fill the name`)
   expect(validationMessageElement).toBeNull()
+})
+
+test('shows name validation message when try to submit with empty name', () => {
+  render(
+    <NewClientForm
+      onClientCreated={() => {}}
+    />
+  )
+
+  fireEvent.click(screen.getByText('Submit'))
+
+  const validationMessageElement = screen.getByText(`Fill the name`)
+  expect(validationMessageElement).toBeInTheDocument()
 })
